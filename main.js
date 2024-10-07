ff443fe87f816173bc5c1839aaafef71c72536ea
Tauane Custodio <tauanesantoscustodio@gmail.com>
2024-10-07 16:35:02 -0300

    feat: add functions to calculate average, clear form and add smiley according to grade

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // --------- variables declaration ---------
 
 const gradeForm = document.getElementById('grade-form');
+const textResult = document.getElementById('result-average');
+const statusResult = document.getElementById('result-status');
+
+let countActivity = 0;
+let averageGrade = 0;
+let sumGrade = 0;
 
 // inputs 
 const activity = document.getElementById('activity-name');
@@ -13,6 +19,14 @@ const tableFooter = document.getElementById('table-footer');
 
 // --------- function declarations ---------
 
+function statusImg(num) {
+    if(num >= 7) {
+        return '<img src="images/aprovado.png" alt="rosto com chapéu de comemoração">';
+    } else {
+        return '<img src="images/reprovado.png" alt="rosto triste">';
+    }
+}
+
 function addRowToTable(activity, grade) {
     const newRow = document.createElement('tr');
     newRow.classList.add('grades-table__row');
@@ -20,19 +34,30 @@ function addRowToTable(activity, grade) {
     newRow.innerHTML = `
         <td class="grades-table__cell">${activity}</td>
         <td class="grades-table__cell">${grade}</td>
-        <td class="grades-table__cell"><img src="images/aprovado.png" alt="rosto com chapéu de comemoração"></td>
+        <td class="grades-table__cell">${statusImg(grade)}</td>
     `;
     tableBody.appendChild(newRow);
 }
 
+function calcAverage(grade) {
+    countActivity++;
+    sumGrade += parseFloat(grade);
+    averageGrade = sumGrade/countActivity;
+    textResult.textContent = averageGrade.toFixed(2);
+    statusResult.innerHTML = statusImg(averageGrade);
+}
 
+function clearForm() {
+    activity.value = '';
+    grade.value = '';
+}
 
 // --------- event listeners ---------
 
 gradeForm.addEventListener('submit', (e) => {
     e.preventDefault();
     addRowToTable(activity.value, grade.value);
+    calcAverage(grade.value);
 
-    activity.value = '';
-    grade.value = '';
+    clearForm();
 });
